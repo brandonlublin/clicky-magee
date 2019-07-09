19daead4305c8cf874a70750963a707d4fc70359
B Lub <brandonlublin@brandons-MacBook-Pro.local>
2019-07-08 20:29:36 -0700

    Updates

---
+++ precache-manifest.67b4209cae2c89739b4ef92f43609a3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90bc1ec18ec0f76d29f1f24c8eaef5d4",
+    "url": "/clicky-magee/index.html"
+  },
+  {
+    "revision": "75c6cc30b0202d5a851f",
+    "url": "/clicky-magee/static/css/main.a5b68034.chunk.css"
+  },
+  {
+    "revision": "e93b33917248aadb9a54",
+    "url": "/clicky-magee/static/js/2.0723b072.chunk.js"
+  },
+  {
+    "revision": "75c6cc30b0202d5a851f",
+    "url": "/clicky-magee/static/js/main.f07deecd.chunk.js"
+  },
+  {
+    "revision": "630eb749c5e8603471a8",
+    "url": "/clicky-magee/static/js/runtime~main.ec9f062e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9b1ab0f1fdd7d112c8aec527585cd9b4.js"
+  "/clicky-magee/precache-manifest.67b4209cae2c89739b4ef92f43609a3f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/clicky-magee/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
